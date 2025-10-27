565e500bca028775500695683de9f98ef2fe3c2c
Kawtar El Gueddari <kawtarelgueddari1@gmail.com>
2025-10-27 15:50:37 +0100

    changements

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ fetch('data.json')
     const container = document.getElementById('jours');
 
     data.forEach(jour => {
-    // Crée un lien vers la page de détail
     const link = document.createElement('a');
     link.href = `jour.html?id=${jour.id}`;
     link.classList.add('carte-jour');
 
-    // Contenu visuel de la carte
     link.innerHTML = `
         <h2>${jour.date}</h2>
         <p>${jour.title}</p>
